b182a5a6563ffd967ff531e6a93ba75226d109d9
YamilMS <yoms.tf@gmail.com>
2022-09-15 19:25:07 +0000

    Second Commit

--- src/models.py
+++ src/models.py
@@ -41,33 +41,6 @@ class Media(Base):
     url= Column(String(250))
     post_id= Column(Integer, ForeignKey('post.id'))
 
-
-
-
-
-"""
-class User(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Post(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-"""
-
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
