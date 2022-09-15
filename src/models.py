c518f26d9c0100e8053cdaa42c4fa52e0eb015a2
YamilMS <yoms.tf@gmail.com>
2022-09-15 19:21:30 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -8,14 +8,52 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
+    __tablename__= 'user'
+    id= Column(Integer, primary_key=True)
+    username= Column(String(250), nullable=False)
+    password= Column(String(250), nullable=False)
+    email= Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__= 'post'
+    id= Column(Integer, primary_key=True)
+    user_id= Column(Integer, ForeignKey('user.id'))
+
+class Follower(Base):
+    __tablename__= 'follower'
+    id= Column(Integer, primary_key=True)
+    user_from_id= Column(Integer, ForeignKey('user.id'))
+    user_to_id= Column(Integer, ForeignKey('user.id'))
+
+
+class Comment(Base):
+    __tablename__= 'comment'
+    id= Column(Integer, primary_key=True)
+    comment_text= Column(String(250))
+    author_id= Column(Integer, ForeignKey('user.id'))
+    post_id= Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__= 'media'
+    id= Column(Integer, primary_key=True)
+    type= Column(String(250))
+    url= Column(String(250))
+    post_id= Column(Integer, ForeignKey('post.id'))
+
+
+
+
+
+"""
+class User(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
+class Post(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
@@ -28,6 +66,7 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+"""
 
 ## Draw from SQLAlchemy base
 try:
